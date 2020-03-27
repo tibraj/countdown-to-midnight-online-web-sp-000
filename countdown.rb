7019be8b7e21feb257a947867082f8dd684c77d5
Taulant Ibraj <tibraj94@gmail.com>
2020-03-27 18:21:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(int)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep(5)
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
