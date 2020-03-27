dc6623549bd61f0bd98b07750b33c128f3a98322
Taulant Ibraj <tibraj94@gmail.com>
2020-03-27 18:06:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  counter = int
+  while counter > 0
+    puts "#{int} SECOND(S)!"
+    sleep (1)
+    counter -= 1
+    break
+    "HAPPY NEW YEAR!"
 end
